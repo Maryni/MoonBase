03d2eb6ed9e8883f909e6a1c92a1a7920683fa35
Maryni <37656934+Maryni@users.noreply.github.com>
2022-01-28 12:31:53 +0200

    git init

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    private BuildType buildType;
+    private Transform transformGetRecources;
+    private Transform transformSetRecources;
+    
+        //write logic to get/set recources
+}
---
+++ Assets/Scripts/BuildingType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildType
+{
+    MoonBaseComponent,
+    MoonIron,
+    MoonSteel
+}
+public class BuildingType : MonoBehaviour
+{
+    
+}
